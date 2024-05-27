f14afbf37a7efebe4f689f5c27988f8f79ed7ad2
Simon57050 <musse.simon14@outlook.com>
2024-05-27 16:32:45 +0200

    Ajout d'images et repositionnement des items

--- template/home.html.php
+++ template/home.html.php
@@ -1,10 +1,10 @@
 
     <div class="contenu">
         <div class="image">
-            <img src="chemin/vers/votre/image.jpg" alt="Description de l'image">
+            <img src="../assets/img/Default_Create_a_visual_representation_of_a_work_scene_an_imag_3.jpg" alt="image page d'accueil">
         </div>
         <div class="carte">
-            <h2>Titre de la carte</h2>
-            <p>Description ou contenu de la carte</p>
+            <p>Avec des options de location flexibles et un service de premier ordre, MNSLoc rend l'acquisition de la dernière technologie simple et sans stress.
+            Explorez notre catalogue dès aujourd'hui et propulsez votre prochain projet vers de nouveaux sommets !</p>
         </div>
     </div>
